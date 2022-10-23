55d56a35d7198072c4ac0df5bf5c251fb0ccb2f5
michaeldyvad <michaeldyvad@gmail.com>
2022-10-23 16:05:08 +0200

    added svelte projects

---
+++ myapp/src/component/Intro.svelte
@@ -0,0 +1,27 @@
+<script>
+  let name = "michael"
+
+  let src = '/tutorial/image.gif';
+
+  import Nested from "./Nested.svelte";
+
+  let string = `this string contains some <strong>HTML!!!</strong>`;
+</script>
+
+<h1>Hello {name.toLocaleUpperCase()}</h1>
+
+<img {src} alt="A {name} dances.">
+
+<p>This is a paragraph.</p>
+
+<Nested/>
+
+<p>{@html string}</p>
+
+<style>
+  p{
+    color: purple;
+    font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+  }
+</style>
